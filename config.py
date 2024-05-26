ec1910123295422f886ee0c04c3db1baf1178e12
kordyak <kordyak@mail.ru>
2024-05-26 18:18:32 +0500

    first

---
+++ config.py
@@ -0,0 +1,9 @@
+api_id = '22475123'
+api_hash = '6084eef3fc47757f5f8847bdc482bc9a'
+bot_token = '6719630568:AAFwrjxHqsiw6GygMtdE3SVIm2F_AqIQwSQ'
+
+group_IA_id = -4246635872
+channel_meduza_id = -1001036240821
+
+channel_out_id = -1002066112684
+channel_in_id = -1002107002771

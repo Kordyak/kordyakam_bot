31068e790601ae3a265a94782ab187ac901b3e8b
kordyak <kordyak@mail.ru>
2024-05-28 13:44:57 +0500

    Second

--- bot.py
+++ bot.py
@@ -3,28 +3,28 @@ import config
 import asyncio
 from telethon import events
 
-import datetime
+from datetime import datetime
 
-from handlers.handler1 import send_message_IA, parsing_old_message
-
-
-def main():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    client = TelegramClient('kord', config.api_id, config.api_hash, loop=loop)
-    client.start()
-
-    @client.on(events.NewMessage(chats=config.channel_url))  # Слушает каналы на сообщение
-    async def handler(event):
-        await send_message_IA(event.message)
-
-    loop.create_task(parsing_old_message(client))
-    return client
+from handlers.handler1 import send_message_IA
 
 
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+client = TelegramClient('kord', config.api_id, config.api_hash, loop=loop)
+client.start()
+
+@client.on(events.NewMessage(chats=config.channel_url))  # Слушает каналы на сообщение
+async def handler(event):
+    print(event.message.date)
+    print(event.message.sender.username)
+    print(event.message.text[:100])
+    print('=' * 100)
+    await send_message_IA(event.message)
+
+# loop.create_task(parsing_old_message(client))
 
 
 if __name__ == '__main__':
-    print(datetime.datetime.now().time())
-    client = main()
+    print(datetime.now().time())
     client.run_until_disconnected()
--- handlers/handler1.py
+++ handlers/handler1.py
@@ -13,27 +13,24 @@ date_check = datetime.today().date()
 
 async def send_message_IA(message: types.Message):  # Отправляет сообщение через бота
     if check_word(message.text):
-        print(message.date)
-        print(message.sender.username)
-        print(message.text[:100])
-        print('=' * 100)
-        # link = f"https://t.me/{message.sender.username}/{message.id}"
-        # await bot.send_message(chat_id=config.group_IA_id,  # Чат ИА id
-        #                        text=f'{link}\n{message.text}',
-        #                        parse_mode=ParseMode.MARKDOWN)
+        print('send_message_IA'*5)
+        print('\n')
+        link = f"https://t.me/{message.sender.username}/{message.id}"
+        await bot.send_message(chat_id=config.group_IA_id,  # Чат ИА id
+                               text=f'{link}\n{message.text}',
+                               parse_mode=ParseMode.MARKDOWN)
 
 
-def check_word(news: str):  # проверка сообщение на слово
-    for i in range(len(config.arr_patterns)):
-        pattern = config.arr_patterns[i]
-        if re.search(pattern, news):
+def check_word(news: str):  # парсинг нововстей на слово
+    for pattern in config.arr_patterns:
+        if re.search(pattern, news.lower()):
             return True
 
 
-async def parsing_old_message(client):  # Парсер старых сообщений
+async def parsing_old_message(client):  # Парсер вчерашних сообщений
     offset_date = datetime.today().date() - timedelta(days=1)
-    for channel_id in config.channel_id:
 
+    for channel_id in config.channel_id:
         iter_messages = client.iter_messages(entity=channel_id, offset_date=offset_date, reverse=True)
 
         async for message in iter_messages:

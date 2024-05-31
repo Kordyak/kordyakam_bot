a2e56aa6c881da480a5a4c1a6f9a444404f4f5b0
kordyak <kordyak@mail.ru>
2024-05-31 07:41:47 +0500

    Second

--- config.py
+++ config.py
@@ -11,20 +11,19 @@ channel_url = [
     'https://t.me/meduzalive',
     'https://t.me/LipsitsIgor',
     'https://t.me/bdtprb',
+    'https://t.me/tvrain',
 ]
 
+'Для парсинга старых сообщений'
 channel_id = [
     -1001036240821,  # meduzalive
     -1001005031786,  # LipsitsIgor
     -1001095950024,  # bdtprb
-    -1001521490869,  # tvrain
+    # -1001521490869,  # tvrain
 ]
 
 key_words = [
-    "главные новости",
-    "главное за",
-    "итоги дня",
-    # "национализаци",
+    "национализаци",
     "мобилизаци",
     "бпла",
     "беспилотник",

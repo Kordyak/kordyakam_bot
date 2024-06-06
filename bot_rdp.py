2bbc55c73901b47be7455602f78e17ecb925ceec
kordyak <kordyak@mail.ru>
2024-06-06 09:03:33 +0500

    from work

--- bot_rdp.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot, Dispatcher, filters, types
-
-import config
-
-bot = Bot(config.BOT_TOKEN2)
-dp = Dispatcher()
-
-@dp.message(lambda message: message.text)
-async def run_rdp(message: types.Message):

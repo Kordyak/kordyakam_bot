33c63c5c12106b9cc276404755b36f212040622d
kordyak <kordyak@mail.ru>
2024-06-08 12:45:09 +0500

    home

--- bot.py
+++ bot.py
@@ -1,15 +1,22 @@
-import aiogram.client.default
-import telethon.events.common
+import asyncio
+import logging
+
 from telethon import TelegramClient, events
 from config import *
-import asyncio
 
 from datetime import datetime
 
 from handlers.handler1 import send_message_IA, check_word, parsing_old_message
-from aiogram import Bot, Dispatcher, F, types, filters, client as aio_cli, enums
+
+from aiogram import Bot, Dispatcher, F, types, filters, enums
+from aiogram.client.default import DefaultBotProperties
 
 import environs
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    style='{',
+                    format='#{levelname:8} | {filename} | {name} | {lineno} : "{message}"')
 
 env = environs.Env()
 env.read_env('.env')
@@ -19,17 +26,18 @@ BOT_TOKEN: str = env.str('BOT_TOKEN')
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
+
 client = TelegramClient('kord2', API_ID, API_HASH, loop=loop)
 client.start()
 
 bot = Bot(token=BOT_TOKEN,
-          default=aio_cli.default.DefaultBotProperties(enums.ParseMode.MARKDOWN))
+          default=DefaultBotProperties(parse_mode=enums.ParseMode.MARKDOWN))
 dp = Dispatcher()
 
 
 @dp.message(filters.Command(commands=['parsing_channel']))
 async def old_news(message: types.Message):
-    print(message.text)
+    logger.info(message.text)
 
     days: int = 1
     arr_text = message.text.split(" ")
@@ -57,7 +65,7 @@ async def handler(event):
 if __name__ == '__main__':
     try:
         date = datetime.now().time().strftime('%H:%M')
-        print(f'Started listening to the news at {date}:')
+        logger.info(f'Started listening to the news at {date}')
         loop.create_task(dp.start_polling(bot))
         client.run_until_disconnected()
     except Exception as e:

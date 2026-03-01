317ed2e2f710c7ba6beca59c7d2914fa003c31ef
kordyak <kordyak@mail.ru>
2026-03-01 11:23:58 +0500

    fix

--- Middlewares/Data.py
+++ Middlewares/Data.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 
 from aiogram import BaseMiddleware
 from aiogram.enums import ChatAction
@@ -14,37 +15,48 @@ class DataMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: TelegramObject, data: dict):
         bot = data.get("bot")
         user_id = None
-        chat_id = None
         username = None
+
 
         # Если это сообщение
         if event.message:
-            user_id = event.message.from_user.id
-            username = event.message.from_user.username
+            user = event.message.from_user
+            user_id = user.id
+            username = user.username
+            first_name = user.first_name
+            last_name = user.last_name
             chat_id = event.message.chat.id
 
         # Если это callback
         elif event.callback_query:
-            user_id = event.callback_query.from_user.id
-            username = event.callback_query.from_user.username
+            user = event.callback_query.from_user
+            user_id = user.id
+            username = user.username
+            first_name = user.first_name
+            last_name = user.last_name
             chat_id = event.callback_query.message.chat.id
 
         if user_id:
             reader = ReaderCache.get_reader(user_id)
+
             data["user_id"] = user_id
             data["reader"] = reader
 
-            # создаем папку пользователя, если не существует
+            # создаем папку пользователя
             user_folder = self.BASE_PATH / str(user_id)
             user_folder.mkdir(parents=True, exist_ok=True)
-            username_file = user_folder / username
-            username_file.write_text("", encoding="utf-8")
 
-        typing_task = asyncio.create_task(
-            self._typing_loop(bot, chat_id)
-        )
+            user_json_path = user_folder / "user.json"
+            user_data = {
+                "username": username,
+            }
+            # если файла нет — создаем
+            if not user_json_path.exists():
+                with open(user_json_path, "w", encoding="utf-8") as f:
+                    json.dump(user_data, f, ensure_ascii=False, indent=4)
 
 
+        typing_task = asyncio.create_task(self._typing_loop(bot, chat_id))
 
         try:
             return await handler(event, data)
